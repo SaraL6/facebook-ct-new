47866c487cc33906e80b7d8e75c7b7d98bf1342b
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2021-09-22 20:34:33 +0100

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav/>
+
+
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+import Nav from './Nav';
+export default {
+    name: "App",
+    components : {
+        Nav
+    }
+};
+</script>
+<style lang=""></style>
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>feqfzeezfezfz</div>
+</template>
+
+
+<script>
+export default {
+    name: "Start"
+};
+</script>
+
+<style lang="">
+</style>
